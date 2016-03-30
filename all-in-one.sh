924264c18c8d2d63364b1af208383f1d52a73a12
Jackie Chen <jackie.chen@news.com.au>
2016-03-30 16:00:44 +1100

    add all-in-one

---
+++ all-in-one.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+clear
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "Running Autosam Version 2 at" `date`
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo ""
+
+echo "============================================================"
+echo " Step one: Test changes in staging if there are any"
+echo "============================================================"
+./test_staging.sh
+
+echo ""
+echo "============================================================"
+echo " Step two: Test changes in production if there are any"
+echo "============================================================"
+./test_production.sh
+
+echo ""
+echo "============================================================"
+echo " Step there: Add new redirect if there are any"
+echo "============================================================"
+./autosam_v2.sh
